4330d4d6b04fe6e43dcb48e9c6aceb9bd1afcb6c
Purplecomrad <74696516+KHROMEKOMRAD@users.noreply.github.com>
2023-03-21 16:37:29 +0000

    added javascipt file to render image grid

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+MEMBERS = [
+  {
+    'id':1,
+    'name':'Krishna Moni Das'
+  },
+  {
+    'id':2,
+    'name':'Milonjyoti Borah'
+  },
+  {
+    'id':3,
+    'name':'Abir Banerjee'
+  },
+  {
+    'id':4,
+    'name':'Amarjeet Boruah'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',Website_dev='Krishna Moni Das',members=MEMBERS)
+
+@app.route("/login")
+def login_page():
+  return render_template('login.html')
 
 
 if __name__ == '__main__':
